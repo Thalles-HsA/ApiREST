677a84ca762b1d65dbb4b2818397c0764e70854d
Thalles-HsA <thsa.henrique@gmail.com>
2023-02-13 15:43:46 -0300

    Finalizando curso de NodeJS com Express e MongoDB

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,15 +3,19 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros);
-        });
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros);
+            });
     };
 
     static listarLivrosPorId = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
+        livros.findById(id) 
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
             if (err) {
                 res.status(400).send({ message: `${err.message} - Id do  livro não localizado.` })
             } else {
@@ -52,9 +56,18 @@ class LivroController {
                 res.status(200).send({ message: 'Livro removido com sucesso' })
             } else {
                 res.status(500).send({ message: err.message })
-            }
-        })
-    }
+            };
+        });
+    };
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
+        });
+    };
+
 };
 
 export default LivroController
